cda8e337088540dc2e8a3d34418f9c4357bff78a
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-28 12:56:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,13 +2,10 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  
+  projects = {}
   html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
-projects = {}
-kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+  kickstarter = Nokogiri::HTML(html)
+
   end
 
 projects
